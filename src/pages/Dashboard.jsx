49edb0200fd10b50336a63de830788a2a372c4eb
Excel Armstrong <gulengfwangshak@gmail.com>
2026-03-20 13:29:25 +0000

    File changes

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -18,6 +18,8 @@ export default function Dashboard() {
         isLoading={isLoading}
       />
 
+      <ActivityNotifications />
+
       <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
         <div className="lg:col-span-2">
           <PriceChart />
